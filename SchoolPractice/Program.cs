ff5f3052fed1c0ae8bd6cccfab5bf901f6c0d041
Andy Magnuson <andymagnuson@gmail.com>
2020-06-08 09:59:35 -0400

    After 5.exercise

--- SchoolPractice/Program.cs
+++ SchoolPractice/Program.cs
@@ -8,7 +8,12 @@ namespace SchoolPractice
         {
             Student andyStudent = new Student("Andy", 1,1, 4.0);
             Console.WriteLine(andyStudent.Name);
-
+            andyStudent.AddGrade(1, 3.0);
+            Console.WriteLine(andyStudent.Gpa);
+            Console.WriteLine(andyStudent.NumberOfCredits);
+            Console.WriteLine(andyStudent.StudentInfo());
+            Console.WriteLine(andyStudent.ToString());
+            Console.WriteLine(andyStudent.Equals(andyStudent));
         }
     }
 }
--- SchoolPractice/Student.cs
+++ SchoolPractice/Student.cs
@@ -7,25 +7,99 @@ namespace SchoolPractice
 {
     public class Student
     {
-        public string Name;
-        public int StudentID;
-        public int NumOfCredits;
-        public double GPA;
-        
-        //private int phone;
-       //public int Phone
-        //{
-        //    set { phone = value; }
-        //}
 
-     public Student(string name, int studentID, int numOfCredits, double gpa)
+        private static int nextStudentId = 1;
+        public string Name { get; set; }
+        private readonly int studentId;
+        public int NumberOfCredits { get; set; }
+        public double Gpa { get; set; }
+
+        public Student(string name, int sId, int numberOfCredits, double gpa)
         {
             Name = name;
-            StudentID = studentID;
-            NumOfCredits = numOfCredits;
-            GPA = gpa;
-            
+            studentId = sId;
+            NumberOfCredits = numberOfCredits;
+            Gpa = gpa;
         }
-       
+
+        public Student(string name, int sId)
+        {
+            Name = name;
+            studentId = sId;
+            NumberOfCredits = 0;
+            Gpa = 0.0;
+        }
+
+        public Student(string name)
+        {
+            Name = name;
+            studentId = nextStudentId;
+            nextStudentId++;
+            NumberOfCredits = 0;
+            Gpa = 0.0;
+        }
+
+        public string StudentInfo()
+        {
+            return (this.Name + " has a GPA of: " + this.Gpa);
+        }
+
+        public void AddGrade(int courseCredits, double grade)
+        {
+            double points = this.Gpa * this.NumberOfCredits;
+            double coursePoints = courseCredits * grade;
+            this.NumberOfCredits = this.NumberOfCredits + courseCredits;
+            this.Gpa = (points + coursePoints) / this.NumberOfCredits;
+           
+        }
+
+        public string GetGradeLevel()
+        {
+            if (this.NumberOfCredits > 89)
+            {
+                return "Senior";
+            } else if (this.NumberOfCredits > 59)
+            {
+                return "Junior";
+            } else if (this.NumberOfCredits > 29)
+            {
+                return "Sophomore";
+            } else
+            {
+                return "Freshman";
+            }
+        }
+
+        public override string ToString()
+        {
+            return Name + ": " + this.GetGradeLevel() + ", GPA: " + Gpa; 
+        }
+
+        public override Boolean Equals(object toBeCompared)
+        {
+
+            if (toBeCompared == this)
+            {
+                return true;
+            }
+
+            if (toBeCompared == null)
+            {
+                return false;
+            }
+
+            if (toBeCompared.GetType() != this.GetType())
+            {
+                return false;
+            }
+
+            Student s = toBeCompared as Student;
+            return s.studentId == studentId;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Name, studentId, NumberOfCredits, Gpa);
+        }
     }
 }
